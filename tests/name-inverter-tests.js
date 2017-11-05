87612f2154841dccbc0b5bb63885623e0e83f379
Sam Meech-Ward <sam@meech-ward.com>
2017-11-05 12:03:11 -0700

    Write pending tests for name inverter using context

--- tests/name-inverter-tests.js
+++ tests/name-inverter-tests.js
@@ -5,5 +5,44 @@ const invertName = require('../name-inverter');
 
 describe('#invertName()', function() {
 
+  context('with undefined name', function() {
+    it('should throw an error');
+  });
 
+  context('with empty name', function() {
+    it('should be empty string');
+  });
+
+  context('with single name', function() {
+    it('should be single name');
+
+    context('and extra spaces around the name', function() {
+      it('should be single name with no spaces');
+    });
+
+  });
+
+  context('with first and last name', function() {
+    it('should be last name, first name');
+
+    context('and extra spaces around the names', function() {
+      it('should be last name, first name with no extra spaces');
+    });
+    
+  });
+
+  context('with honorific', function() {
+    it('should be empty string');
+
+    context('and first name', function() {
+      it('should be <honorific first name>');
+
+      context('and last name', function() {
+        it('should be <honorific last name, first name>');
+      });
+
+    });
+
+  });
+ 
 });
