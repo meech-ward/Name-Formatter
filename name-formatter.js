344e27f586bc8aca09bad8bc56f3347ec5c16b82
Sam Meech-Ward <sam@meech-ward.com>
2017-11-05 12:53:29 -0700

    refactor out the honorific function

---
+++ name-formatter.js
@@ -0,0 +1,43 @@
+class NameFormatter {
+  
+  invertName(name) {
+    if (name === undefined) {
+      throw 'No Name Passed In';
+    }
+
+    let names = name.trim().split(/\s+/);
+
+    let honorific = this.isHonorific(names[0].toLowerCase()); 
+    if (honorific) {
+      honorific = names.shift();
+    }
+    
+    if (names.length == 0) {
+      return "";
+    }
+
+    let invertedName = "";
+    if (names.length == 1) {
+      invertedName = names[0];
+    }
+
+    if (names.length == 2) {
+      invertedName = `${names[1]}, ${names[0]}`;
+    }
+
+    if (honorific) {
+      invertedName = `${honorific} ${invertedName}`;
+    }
+
+    return invertedName;
+  }
+
+  isHonorific(text) {
+    if (text.toLowerCase() === "mr.") {
+      return text;
+    }
+    return null;
+  }
+}
+
+module.exports = NameFormatter;
