20665b77bdfb352f8248dbcc7bec77eec4d43b49
Sam Meech-Ward <sam@meech-ward.com>
2017-11-05 19:38:12 -0700

    Use a composed assertion

--- tests/name-inverter-tests.js
+++ tests/name-inverter-tests.js
@@ -3,74 +3,59 @@ const assert = chai.assert;
 
 const NameFormatter = require('../name-formatter');
 
+
+function assertNameFormatterInvertsName(nameFormatter, inputName, expectedOutput) {
+  assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+}
+
+
 describe('invert name', function() {
 
-  it('should throw an error when name is undefined', function() {
-    const nameFormatter = new NameFormatter();
-    assert.throws(nameFormatter.invertName);
+  let nameFormatter;
+  beforeEach(function() {
+    nameFormatter = new NameFormatter();
+  });
+  
+  context('when name is undefined', function() {
+    it('should throw an error', function() {
+      assert.throws(nameFormatter.invertName);
+    });
   });
 
   it('should return an empty string when passed an empty string', function() {
-    const nameFormatter = new NameFormatter();
-    const inputName = "";
-    const expectedOutput = "";
-    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+    assertNameFormatterInvertsName(nameFormatter, "", "");
   });
 
   it('should return a single name when passed a single name', function() {
-    const nameFormatter = new NameFormatter();
-    const inputName = "name"
-    const expectedOutput = "name";
-    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+    assertNameFormatterInvertsName(nameFormatter, "name", "name");
   });
 
   it('should return a single name when passed a single name with extra spaces', function() {
-    const nameFormatter = new NameFormatter();
-    const inputName = "   name  "
-    const expectedOutput = "name";
-    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+    assertNameFormatterInvertsName(nameFormatter, "   name  ", "name");
   });
 
   it('should return a last-name, first-name when passed a first and last-name', function() {
-    const nameFormatter = new NameFormatter();
-    const inputName = "first last"
-    const expectedOutput = "last, first";
-    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+    assertNameFormatterInvertsName(nameFormatter, "first last", "last, first");
   });
 
   it('should return a last-name, first-name when passed a first and last-name with extra spaces around the names', function() {
-    const nameFormatter = new NameFormatter();
-    const inputName = "  first   last  ";
-    const expectedOutput = "last, first";
-    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+    assertNameFormatterInvertsName(nameFormatter, "  first  last  ", "last, first");
   });
 
   it('should return an empty string when passed Dr.', function() {
-    const nameFormatter = new NameFormatter();
-    const inputName = "Dr.";
-    const expectedOutput = "";
-    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+    assertNameFormatterInvertsName(nameFormatter, "Dr.", "");
   });
 
   it('should return Dr. first-name when passed Dr. first-name', function() {
-    const nameFormatter = new NameFormatter();
-    const inputName = "Dr. first";
-    const expectedOutput = "Dr. first";
-    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+    assertNameFormatterInvertsName(nameFormatter, "Dr. first", "Dr. first");
   });
 
   it('should return a Dr. last-name, first-name when passed Dr. first-name last-name', function() {
-    const nameFormatter = new NameFormatter();
-    const inputName = "Dr. first Last";
-    const expectedOutput = "Dr. Last, first";
-    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+    assertNameFormatterInvertsName(nameFormatter, "Dr. first Last", "Dr. Last, first");
   });
 
   it('should return a Dr. last-name, first-name when passed Dr. first-name last-name with extra spaces', function() {
-    const nameFormatter = new NameFormatter();
-    const inputName = "  Dr.   first   Last  ";
-    const expectedOutput = "Dr. Last, first";
-    assert.equal(nameFormatter.invertName(inputName), expectedOutput);
+    assertNameFormatterInvertsName(nameFormatter, "   Dr.   first   Last  ", "Dr. Last, first");
   });
  
 });
