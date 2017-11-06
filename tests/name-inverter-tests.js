2cf407f7dbdb24d0359eafa6ef31b152f0188147
Sam Meech-Ward <sam@meech-ward.com>
2017-11-05 19:52:25 -0700

    Add context blocks

--- tests/name-inverter-tests.js
+++ tests/name-inverter-tests.js
@@ -16,44 +16,63 @@ describe('invert name', function() {
     nameFormatter = new NameFormatter();
   });
   
-  it('should throw an error when name is undefined', function() {
-    assert.throws(nameFormatter.invertName);
+  context('when name is undefined', function() {
+    it('should throw an error', function() {
+      assert.throws(nameFormatter.invertName);
+    });
   });
+  
+  context('when passed an empty string', function() {
+    it('should return an empty string', function() {
+      assertNameFormatterInvertsName(nameFormatter, "", "");
+    });
+  });
+  
+  context('when passed a single name', function() {
+    it('should return a single name', function() {
+      assertNameFormatterInvertsName(nameFormatter, "name", "name");
+    });
 
-  it('should return an empty string when passed an empty string', function() {
-    assertNameFormatterInvertsName(nameFormatter, "", "");
+    context('with extra spaces', function() {
+      it('should return a single name', function() {
+        assertNameFormatterInvertsName(nameFormatter, "   name  ", "name");
+      });
+    });
   });
+  
+  context('when passed a first and last-name', function() {
+    it('should return a last-name, first-name', function() {
+      assertNameFormatterInvertsName(nameFormatter, "first last", "last, first");
+    });
 
-  it('should return a single name when passed a single name', function() {
-    assertNameFormatterInvertsName(nameFormatter, "name", "name");
+    context('with extra spaces around the names', function() {
+      it('should return a last-name, first-name', function() {
+        assertNameFormatterInvertsName(nameFormatter, "  first  last  ", "last, first");
+      });
+    });
   });
+  
+  context('when passed an honorific', function() {
+    it('should return an empty string', function() {
+      assertNameFormatterInvertsName(nameFormatter, "Dr.", "");
+    });
 
-  it('should return a single name when passed a single name with extra spaces', function() {
-    assertNameFormatterInvertsName(nameFormatter, "   name  ", "name");
-  });
+    context('with a first-name', function() {
+      it('should return honorific first-name', function() {
+        assertNameFormatterInvertsName(nameFormatter, "Dr. first", "Dr. first");
+      });
 
-  it('should return a last-name, first-name when passed a first and last-name', function() {
-    assertNameFormatterInvertsName(nameFormatter, "first last", "last, first");
-  });
+      context('with a last-name', function() {
+        it('should return a honorific last-name, first-name', function() {
+          assertNameFormatterInvertsName(nameFormatter, "Dr. first Last", "Dr. Last, first");
+        });
 
-  it('should return a last-name, first-name when passed a first and last-name with extra spaces around the names', function() {
-    assertNameFormatterInvertsName(nameFormatter, "  first  last  ", "last, first");
+        context('with extra spaces around the names', function() {
+          it('should return a honorific last-name, first-name', function() {
+            assertNameFormatterInvertsName(nameFormatter, "   Dr.   first   Last  ", "Dr. Last, first");
+          });
+        });
+      });
+    });
   });
-
-  it('should return an empty string when passed Dr.', function() {
-    assertNameFormatterInvertsName(nameFormatter, "Dr.", "");
-  });
-
-  it('should return Dr. first-name when passed Dr. first-name', function() {
-    assertNameFormatterInvertsName(nameFormatter, "Dr. first", "Dr. first");
-  });
-
-  it('should return a Dr. last-name, first-name when passed Dr. first-name last-name', function() {
-    assertNameFormatterInvertsName(nameFormatter, "Dr. first Last", "Dr. Last, first");
-  });
-
-  it('should return a Dr. last-name, first-name when passed Dr. first-name last-name with extra spaces', function() {
-    assertNameFormatterInvertsName(nameFormatter, "   Dr.   first   Last  ", "Dr. Last, first");
-  });
- 
 });
